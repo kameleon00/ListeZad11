0368ef46b886b0f38cd236522d44ae36c5d65e7e
Marko Djokic <marko1319@its.edu.rs>
2020-12-30 22:21:40 +0100

    Add project files.

---
+++ ListeZad11/DescStringComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeZad11
+{
+    class DescStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return String.Compare(x, y) * -1;
+        }
+    }
+}
---
+++ ListeZad11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeZad11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, int> gradovi = new SortedList<string, int>(new DescStringComparer())
+            {
+                {"Kraljevo", 40000 },
+                {"Nis", 1400000 },
+                {"Beograd", 2000000 }
+            };
+            foreach (var grad in gradovi)
+                Console.WriteLine(grad);
+
+            Console.ReadLine();
+        }
+    }
+}
